fb661c2c42f79e65355e1432eeb8440f4b51f5e7
Geralex40 <germanacuna011@gmail.com>
2025-09-05 13:26:03 -0600

    con enitits q funcionan

---
+++ conEnitis/src/main/java/saif/casetas/demoOcr/DemoOcrApplication.java
@@ -0,0 +1,14 @@
+package saif.casetas.demoOcr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOcrApplication.class, args);
+	}
+
+
+}
---
+++ conEnitis/src/test/java/saif/casetas/demoOcr/DemoOcrApplicationTests.java
@@ -0,0 +1,13 @@
+package saif.casetas.demoOcr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOcrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
